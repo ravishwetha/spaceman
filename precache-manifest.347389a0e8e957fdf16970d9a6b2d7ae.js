db9588ca877dd0e5b9dd3ea6c2d7112dcde44c15
ravishwetha <ravishwetha@users.noreply.github.com>
2019-06-01 20:50:33 +0800

    Updates

---
+++ precache-manifest.347389a0e8e957fdf16970d9a6b2d7ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bcad9701764337dcd877bc5e0c03168",
+    "url": "/spaceman/index.html"
+  },
+  {
+    "revision": "db132c70b60df48dcf00",
+    "url": "/spaceman/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "43fd306beda9e40c823b",
+    "url": "/spaceman/static/js/2.1b4458ed.chunk.js"
+  },
+  {
+    "revision": "db132c70b60df48dcf00",
+    "url": "/spaceman/static/js/main.9d2340a1.chunk.js"
+  },
+  {
+    "revision": "3c52127a7f940e172c55",
+    "url": "/spaceman/static/js/runtime~main.201ddbd1.js"
+  }
+]);
